d75ac9fab29ffb36a55224ada2f47396037667b2
xtzeng <xiaoti_zeng@126.com>
2014-02-08 09:31:38 +0800

    first commit

---
+++ src/main/java/com/xt/get/App.java
@@ -0,0 +1,13 @@
+package com.xt.get;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
